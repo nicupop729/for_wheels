8ecf4b6826becc77bf335d84813c215186373ffd
Jonathas Tavares <jonathastavares@id.uff.br>
2022-04-05 17:05:34 -0300

    Add cars index and car show views

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Sidebar from './components/sidebar/Sidebar';
 import Cars from './components/cars/Cars';
 import Reserve from './components/reserve/Reserve';
 import MyReservations from './components/my_reservations/MyReservations';
+import ShowCar from './components/cars/ShowCar';
 import './App.css';
 import { getCars } from './redux/cars/car';
 
@@ -17,11 +18,12 @@ const App = () => {
   return (
     <div className="mx-auto mt-8">
       <Sidebar />
-      <main className="text-4xl text-center ml-[10vw]">
+      <main className="text-4xl text-center">
         <Routes>
           <Route path="/" element={<Cars />} />
           <Route path="/reserve" element={<Reserve />} />
           <Route path="/my_reservations" element={<MyReservations />} />
+          <Route path="/car" element={<ShowCar />} />
         </Routes>
       </main>
     </div>
--- src/components/cars/Cars.jsx
+++ src/components/cars/Cars.jsx
@@ -1,3 +1,16 @@
-const Cars = () => <h3>Under Constructions: Cars</h3>;
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Car from './Car';
+
+function Cars() {
+  const cars = useSelector((state) => state.carsReducer);
+  return (
+    <div className="">
+      {cars.map((obj) => (
+        <Car car={obj} key={obj.id} />
+      ))}
+    </div>
+  );
+}
 
 export default Cars;
