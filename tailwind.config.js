73d5cae3ab2eb44d55a1bb4e69053097e37b2704
Nicolae Pop <nicupop729@gmail.com>
2022-04-05 10:14:32 -0400

    Implement tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#ff6363",
+        secondary: {
+          100: "#e2e2d5",
+          200: "#888883",
+        },
+      },
+    },
   },
   plugins: [],
 };
