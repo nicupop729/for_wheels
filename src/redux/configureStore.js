f93119e83c40988de04b46999f06d4d7bbe54fcd
Nicolae Pop <nicupop729@gmail.com>
2022-04-05 21:46:00 -0400

    Configure store for redux

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,11 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import carsReducer from './cars/car';
+import myRentalsReducer from './myReservations/myReservations';
 // Import reducers here
 
 const reducer = combineReducers({
   carsReducer,
-
+  myRentalsReducer,
   // Add reducers here
 });
 
