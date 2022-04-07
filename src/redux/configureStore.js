3e95be4efe0d46c7de12a1cae8e7c362d876592b
Nicolae Pop <nicupop729@gmail.com>
2022-04-07 17:25:54 -0400

    Configure redux for GETing users

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,11 +4,13 @@ import logger from 'redux-logger';
 import carsReducer from './cars/car';
 import myRentalsReducer from './myReservations/myReservations';
 import reservationsReducer from './reservations/reservation';
+import usersReducer from './users/users';
 
 const reducer = combineReducers({
   carsReducer,
   myRentalsReducer,
   reservationsReducer,
+  usersReducer,
 });
 
 const store = createStore(reducer, applyMiddleware(thunk, logger));
